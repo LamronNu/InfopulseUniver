efbe05ee8b4d03707b1471cdd5bb21a8855f02b1
LamronNu <olga.prylypko@gmail.com>
2015-02-14 10:47:39 +0200

    lab 1

--- src/labs/task1_OOP/Order.java
+++ src/labs/task1_OOP/Order.java
@@ -1,9 +1,22 @@
 package labs.task1_OOP;
 
+import labs.task1_OOP.services.OrderService;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Order {
     //fields -- properties
     private String name = "Basic order";
     private String client = "Cat Simon";
+
+    private List<OrderService> services = new ArrayList();
+    private Date dateFrom;
+    private Date dateTo;
+
+    //constructors
+
 
     //getters, setters
 
